4bf5748b8d5eee9c9e6d78161611369c667bc5c4
Jan Oleksik <222529@edu.p.lodz.pl>
2021-11-25 13:13:46 +0100

    added dummy endpoint

---
+++ ppkwu4/src/main/java/com/example/ppkwu4/ControllerConverter.java
@@ -0,0 +1,21 @@
+package com.example.ppkwu4;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerConverter {
+
+    @GetMapping("/api/{from}/{to}")
+    public void convertString(@PathVariable("from") String fromFormat, @PathVariable("to") String targetFormat
+                                , @RequestBody String body)
+    {
+        System.out.println(fromFormat);
+        System.out.println(targetFormat);
+        System.out.println(body);
+    }
+
+
+}
