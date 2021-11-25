0e64b8359d998fcb5da3d291ed7424941b160bf6
Jan Oleksik <222529@edu.p.lodz.pl>
2021-11-25 14:33:04 +0100

    implemented formatString

--- ppkwu4/src/main/java/com/example/ppkwu4/StringFormatUtil.java
+++ ppkwu4/src/main/java/com/example/ppkwu4/StringFormatUtil.java
@@ -8,16 +8,40 @@ import org.json.XML;
 public class StringFormatUtil {
 
     public static String formatString(String from, String to, String text) {
-        if (from.toLowerCase().equals(to.toLowerCase())) {
+        if (from.equalsIgnoreCase(to)) {
             return text;
         }
         String jsonText = covertStringToJsonFormat(from, text);
-        System.out.println(jsonText);
-        return "";
+        String response = null;
+        switch (to) {
+            case "xml":
+                JSONObject jsonObject = new JSONObject(jsonText);
+                StringBuilder res = new StringBuilder("<response>");
+                res.append(XML.toString(jsonObject));
+                res.append("</response>");
+                response = res.toString();
+                break;
+            case "csv":
+                JSONArray jsonArray = new JSONArray();
+                jsonArray.put(new JSONObject(jsonText));
+                response = CDL.toString(jsonArray);
+                break;
+            case "txt":
+                System.out.println("jsON:  "+jsonText);
+                jsonObject = new JSONObject(jsonText);
+                response = String.format("Text: %s\nupperCase: %d\nlowerCase: %d\ndigits: %d\nwhiteCharacters: %d\nspecialCharacters: %d",
+                        jsonObject.get("textToProcess"), jsonObject.get("upperCaseCounter"), jsonObject.get("lowerCaseCounter"), jsonObject.get("digitCounter"),
+                        jsonObject.get("whiteSpaceCounter"), jsonObject.get("specialCharacterCounter"));
+                break;
+            default:
+                break;
+        }
+        System.out.println(response);
+        return response;
     }
 
     private static String covertStringToJsonFormat(String from, String text) {
-        String jsonString = null;
+        String jsonString = text;
         switch (from) {
             case "csv":
                 JSONArray jsonArray = CDL.toJSONArray(text);
@@ -25,7 +49,7 @@ public class StringFormatUtil {
                 break;
             case "xml":
                 JSONObject json = XML.toJSONObject(text);
-                jsonString = json.toString();
+                jsonString = json.getJSONObject("response").toString();
                 break;
             case "txt":
                 final int SPACE_SIZE = 2;
@@ -38,7 +62,7 @@ public class StringFormatUtil {
 
                     jsonObject.put(key, value);
                 }
-                System.out.println(jsonObject);
+                jsonString = jsonObject.toString();
                 break;
             default:
                 break;
