191dd595cd8eef0bec57a0ac07288fd3d843fd87
Jan Oleksik <222529@edu.p.lodz.pl>
2021-11-25 12:50:07 +0100

    init commit

---
+++ ppkwu4/src/main/java/com/example/ppkwu4/Ppkwu4Application.java
@@ -0,0 +1,13 @@
+package com.example.ppkwu4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppkwu4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ppkwu4Application.class, args);
+    }
+
+}
---
+++ ppkwu4/src/test/java/com/example/ppkwu4/Ppkwu4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppkwu4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppkwu4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
